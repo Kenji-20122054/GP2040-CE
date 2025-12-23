a7b09db66b57931a913d7caa23a52014c33d6abd
Kenji-20122054 <141113051+Kenji-20122054@users.noreply.github.com>
2025-12-24 00:46:38 +0900

    Update UARTOutput.h

--- headers/addons/UARTOutput.h
+++ headers/addons/UARTOutput.h
@@ -7,8 +7,8 @@ class UARTOutputAddon : public GPAddon {
 public:
     virtual bool available() override;
     virtual void setup() override;
-    virtual void preprocess() override;
     virtual void process() override;
+    virtual void preprocess() override{}
     // name() は削除しました
 };
 
