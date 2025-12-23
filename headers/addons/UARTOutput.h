102696ad9306752c763adec0a097df1f833e76d5
Kenji-20122054 <141113051+Kenji-20122054@users.noreply.github.com>
2025-12-24 01:07:34 +0900

    Update UARTOutput.h

--- headers/addons/UARTOutput.h
+++ headers/addons/UARTOutput.h
@@ -2,14 +2,23 @@
 #define _UARTOUTPUT_H_
 
 #include "gpaddon.h"
+#include <string>
 
 class UARTOutputAddon : public GPAddon {
 public:
-    virtual bool available() override;
-    virtual void setup() override;
-    virtual void process() override;
-    virtual void preprocess() override{}
-    // name() は削除しました
+    virtual bool available();
+    virtual void setup();
+    virtual void preprocess();
+    virtual void process();
+
+    // ★ログで指摘されていた不足関数 1 (引数の bool が重要)
+    virtual void postprocess(bool);
+
+    // ★ログで指摘されていた不足関数 2
+    virtual void reinit();
+
+    // ★ログで指摘されていた不足関数 3 (ここで中身も実装します)
+    virtual std::string name() { return "UART Output"; }
 };
 
 #endif
