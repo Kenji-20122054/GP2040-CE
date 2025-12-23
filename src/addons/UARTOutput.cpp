e6a1f1535adac67b0f5ccf3ecfd4dc9565ebacf2
Kenji-20122054 <141113051+Kenji-20122054@users.noreply.github.com>
2025-12-24 01:08:20 +0900

    Update UARTOutput.cpp

--- src/addons/UARTOutput.cpp
+++ src/addons/UARTOutput.cpp
@@ -4,6 +4,7 @@
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 
+// 設定
 #define UART_ID uart0
 #define BAUD_RATE 115200
 #define UART_TX_PIN 0
@@ -31,4 +32,14 @@ void UARTOutputAddon::process() {
         uart_putc(UART_ID, gamepad->state.buttons & 0xFF);
         uart_putc(UART_ID, gamepad->state.dpad);
     }
+}
+
+// ★ここが今回追加する重要部分です！★
+
+void UARTOutputAddon::postprocess(bool) {
+    // 処理不要
+}
+
+void UARTOutputAddon::reinit() {
+    // 処理不要
 }
